6ff021373c15d2f3d05e314f99be954171a1a495
Jordi Sala Morales <jordism91@gmail.com>
2020-03-18 08:44:14 +0100

    Add Tests (#7)

---
+++ tests/Fixtures/SortableEntity.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the Runroom package.
+ *
+ * (c) Runroom <runroom@runroom.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Runroom\SortableBehaviorBundle\Tests\Fixtures;
+
+use Runroom\SortableBehaviorBundle\Behaviors\Sortable;
+
+class SortableEntity
+{
+    use Sortable;
+}
---
+++ tests/Unit/SortableTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the Runroom package.
+ *
+ * (c) Runroom <runroom@runroom.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Runroom\SortableBehaviorBundle\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Runroom\SortableBehaviorBundle\Tests\Fixtures\SortableEntity;
+
+class SortableTest extends TestCase
+{
+    protected const POSITION = 42;
+
+    /**
+     * @test
+     */
+    public function itSetsAndGetsPosition()
+    {
+        $sortable = new SortableEntity();
+
+        $sortable = $sortable->setPosition(self::POSITION);
+
+        $this->assertSame(self::POSITION, $sortable->getPosition());
+    }
+}
