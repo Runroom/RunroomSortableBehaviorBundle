f9d8a2b2550a144a11da4655e5e17b05525b04bf
Jordi Sala Morales <jordism91@gmail.com>
2020-06-16 11:56:00 +0200

    Simplify integration test and upgrade packages (#49)

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -23,42 +23,41 @@ final class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('runroom_sortable_behavior');
-
         $rootNode = $treeBuilder->getRootNode();
 
-        $rootNode
-            ->children()
-                ->scalarNode('position_handler')
-                    ->cannotBeEmpty()
-                    ->defaultValue(GedmoPositionHandler::class)
-                    ->validate()
-                        ->ifTrue(function ($config) {
-                            return !is_a($config, PositionHandlerInterface::class, true);
-                        })
-                        ->thenInvalid('%s must implement ' . PositionHandlerInterface::class)
+        $rootNode->children()
+            ->scalarNode('position_handler')
+                ->cannotBeEmpty()
+                ->defaultValue(GedmoPositionHandler::class)
+                ->validate()
+                    ->ifTrue(function ($config) {
+                        return !is_a($config, PositionHandlerInterface::class, true);
+                    })
+                    ->thenInvalid('%s must implement ' . PositionHandlerInterface::class)
+                ->end()
+            ->end()
+            ->arrayNode('position_field')
+                ->addDefaultsIfNotSet()
+                ->children()
+                    ->scalarNode('default')
+                        ->defaultValue('position')
+                    ->end()
+                    ->arrayNode('entities')
+                        ->prototype('scalar')->end()
                     ->end()
                 ->end()
-                ->arrayNode('position_field')
-                    ->addDefaultsIfNotSet()
-                    ->children()
-                        ->scalarNode('default')
-                            ->defaultValue('position')
-                        ->end()
-                        ->arrayNode('entities')
-                            ->prototype('scalar')->end()
+            ->end()
+            ->arrayNode('sortable_groups')
+                ->addDefaultsIfNotSet()
+                ->children()
+                    ->arrayNode('entities')
+                        ->useAttributeAsKey('name')
+                        ->prototype('variable')
                         ->end()
                     ->end()
                 ->end()
-                ->arrayNode('sortable_groups')
-                    ->addDefaultsIfNotSet()
-                    ->children()
-                        ->arrayNode('entities')
-                            ->useAttributeAsKey('name')
-                            ->prototype('variable')
-                            ->end()
-                        ->end()
-                    ->end()
-                ->end();
+            ->end()
+        ->end();
 
         return $treeBuilder;
     }
