a1d503fce6fe1a897868677278e8ed3c5a3e2187
a.dmitryuk <a.dmitryuk@movavi.com>
2022-09-06 11:34:22 +0700

    Possible to use trait

--- src/Admin/AbstractSortableAdmin.php
+++ src/Admin/AbstractSortableAdmin.php
@@ -14,8 +14,6 @@ declare(strict_types=1);
 namespace Runroom\SortableBehaviorBundle\Admin;
 
 use Sonata\AdminBundle\Admin\AbstractAdmin;
-use Sonata\AdminBundle\Route\RouteCollection;
-use Sonata\AdminBundle\Route\RouteCollectionInterface;
 
 /**
  * @template T of object
@@ -24,21 +22,5 @@ use Sonata\AdminBundle\Route\RouteCollectionInterface;
  */
 abstract class AbstractSortableAdmin extends AbstractAdmin
 {
-    /**
-     * @param mixed[] $sortValues
-     */
-    protected function configureDefaultSortValues(array &$sortValues): void
-    {
-        $sortValues['_sort_by'] = 'position';
-    }
-
-    /**
-     * @todo: Simplify this when dropping support for Sonata 3
-     *
-     * @param RouteCollection|RouteCollectionInterface $collection
-     */
-    protected function configureRoutes(object $collection): void
-    {
-        $collection->add('move', $this->getRouterIdParameter() . '/move/{position}');
-    }
+    use SortableAdminTrait;
 }
---
+++ src/Admin/SortableAdminTrait.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace Runroom\SortableBehaviorBundle\Admin;
+
+use Sonata\AdminBundle\Route\RouteCollection;
+use Sonata\AdminBundle\Route\RouteCollectionInterface;
+
+trait SortableAdminTrait
+{
+    /**
+     * @param mixed[] $sortValues
+     */
+    protected function configureDefaultSortValues(array &$sortValues): void
+    {
+        $sortValues['_sort_by'] = 'position';
+    }
+
+    /**
+     * @todo: Simplify this when dropping support for Sonata 3
+     *
+     * @param RouteCollection|RouteCollectionInterface $collection
+     */
+    protected function configureRoutes(object $collection): void
+    {
+        $collection->add('move', $this->getRouterIdParameter() . '/move/{position}');
+    }
+
+    abstract function getRouterIdParameter(): string;
+}
